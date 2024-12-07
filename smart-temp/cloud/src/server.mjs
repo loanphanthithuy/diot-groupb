3cdd3f9fe047d9c86af582c4116f12bcd9588d65
Huy Khoa Nguyen <nguyenhuykhoa2505@gmail.com>
2024-12-07 23:10:51 +0200

    Changes and adds file

--- smart-temp/cloud/src/server.mjs
+++ smart-temp/cloud/src/server.mjs
@@ -7,7 +7,7 @@ const app = express();
 const HOST = "127.0.0.1";
 const PORT = 3001;
 
-// 3. InfluxDB Configuration
+// 2. InfluxDB Configuration
 const DB_HOST = "http://localhost:8086";
 // API ORG, BUCKET, TOKEN from Loan's local
 // const DB_ORG = "LAB";
@@ -16,7 +16,7 @@ const DB_HOST = "http://localhost:8086";
 
 // API ORG, BUCKET, TOKEN from Khoa's local
 const DB_ORG = "LAB";
-const DB_BUCKET = "db_api"
+const DB_BUCKET = "db_api";
 const DB_TOKEN = "p-TBWodnZyVnpqFH6dM-pdqI4g8o-qbKOrzEwtPXTC2v2hIAajsQWIYuxvYUZSD6fWAlOJ9L16cG1L-N0UqTTw==";
 
 // Initialize InfluxDB Client
@@ -29,8 +29,8 @@ DB_WRITE_POINT.useDefaultTags({ app: "query-param-app" });
 const QUERY_API = DB_CLIENT.getQueryApi(DB_ORG);
 
 console.log("Connected to InfluxDB");
- 
-// 4. Define '/data' endpoint to write the Data to DB
+
+// 3. Define '/data' endpoint to write the Data to DB
 // http://51.120.10.183/api/v1/data?value=26.0 => to database // Previously, for only 1 sensor
 // http://51.120.10.183/api/v1/data?value=25.0&location=kitchen // Now, for multiple sensors
 app.get('/data', validateTemperature, async (req, res) => {
@@ -39,7 +39,7 @@ app.get('/data', validateTemperature, async (req, res) => {
         res.status(400).send('Missing query parameter "value" or "location"');
         return;
     }
-
+    
     try {
         // Log received value
         // console.log(`Received value from Pico W: ${value}`); // with 1 sensor
@@ -66,15 +66,15 @@ function validateTemperature(req, res, next) {
         res.status(400).send('Missing query parameter "value" or "location"');
         return;
     }
-
+    
     const minTemperature = -50; 
     const maxTemperature = 50; 
-
+    
     // Check if the temperature is within the valid range
     if (value < minTemperature || value > maxTemperature) {
         return res.status(400).json({ error: 'Invalid temperature value' });
     }
-
+    
     // If valid, proceed to the next middleware or route handler
     next();
 }
@@ -82,20 +82,20 @@ function validateTemperature(req, res, next) {
 // Define endpoint to retrieve the Data
 
 app.get('/temp', async (req, res) => {
-    const location  = req.query;
+    const location  = req.query.location;
     if (!location) {
         res.status(400).send('Missing query parameters "location"');
         return;
     }
     const query = `
-        from(bucket: "${DB_BUCKET}")
-        |> range(start: 0)
-        |> filter(fn: (r) => r._measurement == "qparams")
-        |> filter(fn: (r) => r._field == "value")
-        |> filter(fn: (r) => r.location == "${location}")
-        |> keep(columns: ["_time", "_value"])
+    from(bucket: "${DB_BUCKET}")
+    |> range(start: 0)
+    |> filter(fn: (r) => r._measurement == "qparams")
+    |> filter(fn: (r) => r._field == "value")
+    |> filter(fn: (r) => r.location == "${location}")
+    |> keep(columns: ["_time", "_value"])
     `;
-
+    
     try {
         const data = [];
         const rows = await QUERY_API.collectRows(query);
@@ -109,7 +109,10 @@ app.get('/temp', async (req, res) => {
     }
 });
 
-// 5. Start the server
+// 4. Start the server
 app.listen(PORT, HOST, () => {
     console.log(`Cloud API listening at http://${HOST}:${PORT}`);
 });
+
+// For Unit Testing
+export default app;
